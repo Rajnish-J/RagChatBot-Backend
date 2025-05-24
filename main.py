760ff30fb47b752dcd693dc170b05cb7c8ae1acb
Rajnish-J <rajnishofficial02@gmail.com>
2025-05-25 00:08:18 +0530

    Refactor FastAPI application: add chatBot endpoint and improve prompt handling

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
+
+# * Import statements for langchains
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda, RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
 
-from fastapi import FastAPI
+# * Import statements for FastAPI 
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
 
+# * Env file loading imports
 import os
 from dotenv import load_dotenv
 
+# * models imports
 from models.promptRequestModel import PromptRequest
 
+# * Loads the env file to read the gemini api ket
 load_dotenv()
 
 API_KEY = os.getenv("GEMINI_API_KEY")
+
+# * Initialize the FastAPI application
+app = FastAPI()
 
 output_parser = StrOutputParser()
 
@@ -21,9 +32,33 @@ llm = ChatGoogleGenerativeAI(
     google_api_key=API_KEY
 )
 
-app = FastAPI()
+message = PromptTemplate(
+    input_variables=['topic'],
+    template="Give me a para about {topic}"
+)
 
-@app.post("/getJoke")
-async def getJoke(request: PromptRequest):
-    response = llm.invoke("Tell me a Joke")
-    return JSONResponse(content={"response": str(response.content)})
+summary = PromptTemplate(
+    input_variables=['input'],
+    template="Give me a summary of {input} in two lines"
+)
+
+wrap_input = RunnableLambda(lambda x: {"input": x})
+
+chain = (
+    {"topic": RunnablePassthrough()}
+    | message
+    | llm
+    | output_parser
+    | wrap_input
+    | summary
+    | llm
+    | output_parser
+)
+
+@app.post("/chatBot")
+async def chatBot(request: PromptRequest):
+    try:
+        response = chain.invoke({"topic": request})
+        return JSONResponse(content={"topic": request.prompt, "response": response})
+    except Exception as error:
+        raise HTTPException(status_code=500, detail=error)
